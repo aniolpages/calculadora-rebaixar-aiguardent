fc55aafbef005d0fd922be6607a95e2bf901063a
Aniol Pagès i Selga <47257568+aniolpages@users.noreply.github.com>
2022-08-26 21:27:35 +0200

    Millores de disseny i usabilitat

--- script.js
+++ script.js
@@ -15,24 +15,26 @@ function novesDades() {
         i = document.getElementById("n_in").value;
     i1 = "" !== e, i2 = "" !== t, i3 = "" !== n, i4 = "" !== i;
     t = e * (t / "100") / (i / "100"), n *= t / "1000", e = t - e - "0.6" * n, n = Math.round(n), e = Math.round(e), t = Math.round(t);
-    document.getElementById("f_a").innerHTML = e + " ml", document.getElementById("f_s").innerHTML = n + " g", document.getElementById("f_v").innerHTML = "Per aconseguir un volum de <b>" + t + " ml</b> a una graduació de <b>" + i + "%</b>. <br><br>Recomanem utilitzar aigua de mineralització molt dèbil. Per minimitzar l'enterboliment de la ratafia, afegiu l'aiguardent sobre el xarop.", 1 == i1 && 1 == i2 && 1 == i3 && 1 == i4 && 0 == fet && (fet = !0, document.getElementById("r").style.display = "block", document.getElementById("t").style.height = Math.floor(document.getElementById("t").offsetHeight + document.getElementById("r").offsetHeight) + "px", setTimeout(function() {
-        document.getElementById("r").style.opacity = 1
-    }, 150))
+    if (isFinite(n) && isFinite(e) && isFinite(t) && !isNaN(e) && !isNaN(n) && !isNaN(t) && e != Infinity && n != Infinity && t != Infinity && document.getElementById("a_in").value.length > 0 && document.getElementById("b_in").value.length > 0 && document.getElementById("g_in").value.length > 0 && document.getElementById("n_in").value.length > 0) {
+        document.getElementById("f_a").innerHTML = e + " ml", document.getElementById("f_s").innerHTML = n + " g", document.getElementById("f_v").innerHTML = "Per aconseguir un volum de <b>" + t + " ml</b> a una graduació de <b>" + i + "%</b>. <br><br>Recomanem utilitzar aigua de mineralització molt dèbil. Per minimitzar l'enterboliment de la ratafia, afegiu l'aiguardent sobre el xarop.", 1 == i1 && 1 == i2 && 1 == i3 && 1 == i4 && 0 == fet && (fet = !0, document.getElementById("r").style.display = "block", document.getElementById("t").style.height = Math.floor(document.getElementById("t").offsetHeight + document.getElementById("r").offsetHeight) + "px", setTimeout(function () {
+            document.getElementById("r").style.opacity = 1
+        }, 150))
+    }
 }
 
 function setInputFilter(t, n) {
-    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(e) {
-        t.addEventListener(e, function() {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (e) {
+        t.addEventListener(e, function () {
             n(this.value) ? (this.oldValue = this.value, this.oldSelectionStart = this.selectionStart, this.oldSelectionEnd = this.selectionEnd) : this.hasOwnProperty("oldValue") && (this.value = this.oldValue, this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd))
         })
     })
 }
-setInputFilter(document.getElementById("b_in"), function(e) {
+setInputFilter(document.getElementById("b_in"), function (e) {
     return /^\d*\.?\d*$/.test(e)
-}), setInputFilter(document.getElementById("a_in"), function(e) {
+}), setInputFilter(document.getElementById("a_in"), function (e) {
     return /^\d*\.?\d*$/.test(e)
-}), setInputFilter(document.getElementById("g_in"), function(e) {
+}), setInputFilter(document.getElementById("g_in"), function (e) {
     return /^\d*\.?\d*$/.test(e)
-}), setInputFilter(document.getElementById("n_in"), function(e) {
+}), setInputFilter(document.getElementById("n_in"), function (e) {
     return /^\d*\.?\d*$/.test(e)
 });
